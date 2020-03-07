10e4e0318b07b1d3d31a7cfcccd21022f64e5300
Erfan Abdi <erfangplus@gmail.com>
2020-03-07 05:30:13 +0330

    whyred: Don’t enforce rro for all overlays
    * Breaking /product/overlay
    * Also this makes many platform specific overlays with current system API on vendor partition which breaks newer android version compatibilities (broken stuffs in gsis)
    
    Change-Id: I45eb18b0754726fb4f779521a0245dfcb1259b17

--- device.mk
+++ device.mk
@@ -11,8 +11,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
     $(DEVICE_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := *
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH) \

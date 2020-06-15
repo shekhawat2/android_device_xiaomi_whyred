0f37c8007ce543e15557030af63d51fcb39e834c
Nitin Shivpure <nshivpur@codeaurora.org>
2020-06-15 10:48:05 +0530

    whyred: bt: Add vendor.qti.hardware.btconfigstore@2.0 into product package
    Add vendor.qti.hardware.btconfigstore@2.0 into product package.
    
    CRs-fixed: 2739162

--- device.mk
+++ device.mk
@@ -128,7 +128,8 @@ PRODUCT_BOOT_JARS += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \

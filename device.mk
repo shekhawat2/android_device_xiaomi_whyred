b9d6eb4e66b98ad163faaab85b770156cb81c573
sashascurtu <sasha.scurtu@gmail.com>
2021-06-24 20:20:22 +0000

    whyred: Build mock power stats HAL and label it
    Fixes the following logspam:
    * Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device manifest.
    * BatteryStatsService: Unable to load Power Hal or power.stats HAL
    
    Change-Id: I721b832f4abf1f31bc5e2a9199cb31f757d9b51b

--- device.mk
+++ device.mk
@@ -359,7 +359,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr
+    android.hardware.power-service.xiaomi-libperfmgr \
+    android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \

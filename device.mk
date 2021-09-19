020d6987281e58660742087df64e671936b8055e
Shekhawat2 <anandsingh215@yahoo.com>
2021-09-19 12:27:30 +0530

    whyred: Build power stats hal and label it
     * Fix: Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device manifest.

--- device.mk
+++ device.mk
@@ -338,7 +338,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr
+    android.hardware.power-service.xiaomi-libperfmgr \
+    android.hardware.power.stats@1.0-service.mock
 
 # Powerhint
 PRODUCT_COPY_FILES += \

3ce75202ae3ec5cde59fad6420e9c49eaa933a4b
Anand Shekhawat <anandsingh215@yahoo.com>
2021-10-10 17:36:59 +0000

    whyred: Drop XiaomiDoze

--- device.mk
+++ device.mk
@@ -189,10 +189,6 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.9.vendor \
     vendor.display.config@2.0.vendor
 
-# Doze
-PRODUCT_PACKAGES += \
-    XiaomiDoze
-
 # DPM
 PRODUCT_PACKAGES += \
     libshim_dpmframework

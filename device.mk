4c16cd27f71bb1cde7a575eed644d426d9946c59
orges <me@orgesified.ml>
2021-10-13 16:39:40 +0000

    whyred: add display config file
     * DisplayPowerController[0]: Screen brightness nits configuration is unavailable; falling back
     * DisplayDeviceConfig: requesting nits when no mapping exists.

--- device.mk
+++ device.mk
@@ -182,6 +182,10 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.9.vendor \
     vendor.display.config@2.0.vendor
 
+# Display config
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/display_id_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_0.xml
+
 # Doze
 PRODUCT_PACKAGES += \
     XiaomiDoze

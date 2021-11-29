ee9f7fe20530da62aa2ad6d0b3fe3c2248bb6bff
Anand Shekhawat <anandsingh215@yahoo.com>
2021-11-29 10:09:22 +0000

    whyred: Switch to QTI vibrator

--- device.mk
+++ device.mk
@@ -413,9 +413,7 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
 # Vibrator
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl:64 \
-    android.hardware.vibrator@1.0-service
+$(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
 
 # Wifi
 PRODUCT_PACKAGES += \

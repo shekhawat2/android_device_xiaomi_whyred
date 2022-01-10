e93710a5597b5098e3689141b5f23ad78b8934b8
Anand Shekhawat <anandsingh215@yahoo.com>
2022-01-10 07:16:52 +0000

    whyred: update rootdir from LA.UM.10.2.1.r1-03200-sdm660.0

--- device.mk
+++ device.mk
@@ -269,15 +269,17 @@ PRODUCT_COPY_FILES += \
 # Init
 PRODUCT_PACKAGES += \
     init.class_main.sh \
-    init.msm.usb.configfs.rc \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
+    init.qcom.sensors.sh \
+    init.qti.dcvs.sh \
     init.qcom.rc \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
     init.recovery.qcom.rc \
     init.target.rc \
+    init.target_dap.rc \
     ueventd.qcom.rc \
     fstab.qcom \
     init.device.rc

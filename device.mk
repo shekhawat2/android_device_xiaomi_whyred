5a8190a098aaf2655ab8499da97d5252c976d7f9
Anand Shekhawat <anandsingh215@yahoo.com>
2022-01-17 17:00:48 +0000

    whyred: build vendor.display.config varients

--- device.mk
+++ device.mk
@@ -178,7 +178,8 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl \
     android.frameworks.displayservice@1.0 \
-    vendor.display.config@1.1
+    vendor.display.config@1.9.vendor \
+    vendor.display.config@2.0.vendor
 
 # DPM
 PRODUCT_PACKAGES += \

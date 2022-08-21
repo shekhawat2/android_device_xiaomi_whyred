3e3abe0eb5baba5003be214bb8b6923add0b80a0
Anand Shekhawat <anandsingh215@yahoo.com>
2022-08-21 16:52:17 +0530

    whyred: Switch bluetooth audio to aidl

--- device.mk
+++ device.mk
@@ -84,7 +84,7 @@ PRODUCT_PACKAGES += \
     libvolumelistener
 
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.audio@2.1-impl:32 \
+    android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default
 
 PRODUCT_PACKAGES += \

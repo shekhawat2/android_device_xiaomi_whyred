decc0ff48d5eef7a00d28b9baa11a52e6d9f2ed7
Anand Shekhawat <anandsingh215@yahoo.com>
2022-08-30 16:46:25 +0530

    Revert "whyred: Speed profile services and wifi-service to reduce RAM and storage"
    This reverts commit 48deb2977b58d39f01de887aa21727ee4b3b4fe7.

--- device.mk
+++ device.mk
@@ -426,13 +426,6 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service \
     android.frameworks.sensorservice@1.0.vendor
 
-# Speed profile services and wifi-service to reduce RAM and storage
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
-PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
-USE_DEX2OAT_DEBUG := false
-PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
-PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
-
 # Telephony
 PRODUCT_PACKAGES += \
     ipacm \

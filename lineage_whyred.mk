9a8fa21583deb34ac0a08a8687e634ae1255cf1f
Anand Shekhawat <anandsingh215@yahoo.com>
2022-10-27 04:26:17 +0000

    whyred: add Rice Flags

--- lineage_whyred.mk
+++ lineage_whyred.mk
@@ -25,6 +25,12 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from whyred device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+# Rice Stuff
+WITH_GMS := true
+RICE_CHIPSET := "SDM636"
+RICE_MAINTAINER := "Shekhawat2"
+SUSHI_BOOTANIMATION := 1080
+
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
